5bc4332f545f9b7b0dfa0670b055204d9160da33
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2024-09-07 18:54:06 +0800

    hw6^.^

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexToDelete = this.#items.indexOf(itemToRemove);
+    if (indexToDelete === -1) {
+      return;
+    }
+    this.#items.splice(indexToDelete, 1);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
